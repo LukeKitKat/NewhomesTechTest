88917cc1d8d972514dbdcc8315ca5bfd2a88bf5f
Luke.C <lukechalkley@outlook.com>
2024-12-22 16:04:45 +0000

    Basic addition functionality complete.

--- Presentation.Newhomes/Pages/NewhomesMain.razor.cs
+++ Presentation.Newhomes/Pages/NewhomesMain.razor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Server.Newhomes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace Presentation.Newhomes.Pages
 {
     public partial class NewhomesMain
     {
+        public NumericalModel Model { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+
+        public void CalculateNumber()
+        {
+            Model.Result = Model.Number1 + Model.Number2;
+            StateHasChanged();
+        }
     }
 }
---
+++ Server.Newhomes/Models/NumericalModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Newhomes.Models
+{
+    public class NumericalModel
+    {
+        public int Id { get; set; }
+
+        [Required, NotNull]
+        public int? Number1 { get; set; }
+
+        [Required, NotNull]
+        public int? Number2 { get; set; }
+
+        public int? Result { get; set; }
+    }
+}
